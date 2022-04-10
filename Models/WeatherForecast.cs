e94a895f1d2641e20c9ddf084fc43c61501b3a76
sreeragkutti <sreerag801@outlook.com>
2022-04-10 20:43:16 +0530

    Add project files.

---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PowerEFCore.Models
+{
+    public class WeatherForecast
+    {
+        public string Summary { get;  set; }
+        public int TemperatureC { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
